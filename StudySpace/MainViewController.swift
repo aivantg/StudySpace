d7a401df2667653cee64a46810fcd731ba54baba
Aivant Goyal <aivantg_developer@yahoo.com>
2017-10-27 21:53:16 -0700

    Added Base Navigation Frame

--- StudySpace/MainViewController.swift
+++ StudySpace/MainViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
