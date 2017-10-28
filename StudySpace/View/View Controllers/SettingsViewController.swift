88f7d46c20ebc656418d5a4bc169536710305915
Omkar Waingankar <halliday@Omkars-MacBook-Pro.local>
2017-10-28 02:34:36 -0700

    Fixed Settings

--- StudySpace/View/View Controllers/SettingsViewController.swift
+++ StudySpace/View/View Controllers/SettingsViewController.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
-class SettingsViewController: MainViewController {
+class SettingsViewController: UITableViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
